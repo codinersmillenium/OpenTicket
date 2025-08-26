bda42071dc23522d483c22c3f7bfacfbe7cd63f8
codinersmillenium <>
2025-08-27 07:17:20 +0800

    deploy update agent local

--- src/fetch/api.py
+++ src/fetch/api.py
@@ -1,7 +1,7 @@
 import json
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
-from agent_inject import process_query_simple  # pakai versi simple
+from agent_local import process_query_simple  # pakai versi simple
 
 app = FastAPI()
 
